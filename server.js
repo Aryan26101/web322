d6626a1667402dcdd7a367080c0f8905950907a0
Aryan26101 <aryanrathod2610@gmail.com>
2023-01-19 13:43:52 -0500

    Seneca academic policy added

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Aryan Rakeshbhai Rathod Student ID:129796215 Date: 19-01-2023
+*
+*  Cyclic Web App URL: https://easy-miniskirt-crab.cyclic.app/
+*
+*  GitHub Repository URL: https://github.com/Aryan26101/web322/tree/main
+*
+********************************************************************************/ 
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
